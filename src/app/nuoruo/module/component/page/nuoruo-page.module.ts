162bb3bc0e27d1013b646f083545a951aab480ec
mayiwen <i@mayiwen.com>
2024-01-24 18:08:28 +0800

    2024-01-24 修改样式

--- src/app/nuoruo/module/component/page/nuoruo-page.module.ts
+++ src/app/nuoruo/module/component/page/nuoruo-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NuoruoPageComponent } from './nuoruo-page.component';
 import { NuoruoButtonModule } from '../button/nuoruo-button.module';
+import { NuoruoInputModule } from '../input/nuoruo-input.module';
 const components = [
   NuoruoPageComponent
 ];
@@ -10,7 +11,7 @@ const components = [
   declarations: [...components],
   exports: [...components],
   // imports: [CommonModule, FormsModule, ReactiveFormsModule, MywButtonModule, MywInputModule],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, NuoruoButtonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NuoruoButtonModule, NuoruoInputModule],
   bootstrap: [],
   providers: [],
 })
